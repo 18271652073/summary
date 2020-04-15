6edca59be5ec6162bc30f15c7bbf3b31c11b97ae
dukaixiang <827739960@qq.com>
2020-04-15 17:04:14 +0800

    0415  测试实体转换空字段的值给空

--- src/main/java/com/test/summary/dom/kafka/Message.java
+++ src/main/java/com/test/summary/dom/kafka/Message.java
@@ -1,5 +1,8 @@
 package com.test.summary.dom.kafka;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 
 import java.util.Date;
@@ -9,6 +12,7 @@ import java.util.Date;
  * @date 2020/4/10.
  */
 @Data
+@JsonSerialize(include= JsonSerialize.Inclusion.NON_NULL)
 public class Message {
 
     private String id;    //id
@@ -17,5 +21,14 @@ public class Message {
 
     private Date sendTime;  //时间戳
 
+    public static void main(String[] args) {
+        //测试实体转换空字段的值给空
+        Message message=new Message();
+        message.setMsg("123");
+        String str = JSON.toJSONString(message, new SerializerFeature[]{SerializerFeature.DisableCircularReferenceDetect});
+        String str2 = JSON.toJSONString(message.toString());
+        System.out.println(str);
+        System.out.println(str2);
+    }
 }
 
