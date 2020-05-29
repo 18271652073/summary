776408862672365e89b822cebb65ff718a93257b
dukaixiang <827739960@qq.com>
2020-05-29 16:33:54 +0800

    0529 fastjson的转化

--- src/main/java/com/test/summary/dom/kafka/Message.java
+++ src/main/java/com/test/summary/dom/kafka/Message.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Date;
 
 /**
@@ -13,7 +14,7 @@ import java.util.Date;
  */
 @Data
 @JsonSerialize(include= JsonSerialize.Inclusion.NON_NULL)
-public class Message {
+public class Message implements Serializable {
 
     private String id;    //id
 
@@ -23,11 +24,21 @@ public class Message {
 
     public static void main(String[] args) {
         //测试实体转换空字段的值给空
-        Message message=new Message();
+        Message message = new Message();
         message.setMsg("123");
+        message.setSendTime(new Date());
         String str = JSON.toJSONString(message, new SerializerFeature[]{SerializerFeature.DisableCircularReferenceDetect});
+        //WriteMapNullValue  空值默认给null
+        //WriteNullStringAsEmpty  如果是字符串类型为空的参数赋值""
+        //WriteNullNumberAsZero   空数值默认给0
+        //WriteDateUseDateFormat  时间修改格式
+        //1.单个关闭 JSON.toJSONString(object, SerializerFeature.DisableCircularReferenceDetect);
+        //2.全局配置关闭 JSON.DEFAULT_GENERATE_FEATURE |= SerializerFeature.DisableCircularReferenceDetect.getMask();
+        JSON.DEFFAULT_DATE_FORMAT = "yyyy-MM-dd";//与WriteDateUseDateFormat搭配使用
+        String str1 = JSON.toJSONString(message, new SerializerFeature[]{SerializerFeature.UseISO8601DateFormat, SerializerFeature.WriteNullStringAsEmpty, SerializerFeature.WriteDateUseDateFormat});
         String str2 = JSON.toJSONString(message.toString());
         System.out.println(str);
+        System.out.println(str1);
         System.out.println(str2);
     }
 }
